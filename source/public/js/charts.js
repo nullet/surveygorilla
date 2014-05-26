8e06667e58acfd058a4984c42d5f7a55cbb583b7
Gustavo Puglia <gpugliac@gmail.com>
2014-05-26 10:23:26 -0500

    adds multiple graphics in results (buggy)

--- source/public/js/charts.js
+++ source/public/js/charts.js
@@ -1,55 +1,70 @@
 $(document).ready(function() {
- var margin = {top: 20, right: 30, bottom: 30, left: 30},
-    width = 600 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+  var questions = $('.question-id').map(function() { return $(this).text(); }).get();
+    
+    var margin = {top: 20, right: 30, bottom: 30, left: 30},
+      width = 600 - margin.left - margin.right,
+      height = 500 - margin.top - margin.bottom;
 
-var x = d3.scale.ordinal()
-    .rangeRoundBands([0, width], .1);
+    var x = d3.scale.ordinal()
+        .rangeRoundBands([0, width], .1);
 
-var y = d3.scale.linear()
-    .range([height, 0]);
+    var y = d3.scale.linear()
+        .range([height, 0]);
 
-var xAxis = d3.svg.axis()
-    .scale(x)
-    .orient("bottom");
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom");
 
-var yAxis = d3.svg.axis()
-    .scale(y)
-    .orient("left");
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left");
 
-var chart = d3.select(".chart")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+    var index = 0
 
-var question = parseInt($('#question-id').text());
+    var chart = d3.selectAll(".chart").each(function(){
+        $(this).attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-d3.tsv(question + "/results.tsv", type, function(error, data) {
-  x.domain(data.map(function(d) { return d.name; }));
-  y.domain([0, d3.max(data, function(d) { return d.value; })]);
+    console.log(questions)
+    var question = parseInt(questions[index]);
+    console.log("Question:" + question);
 
-  chart.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis);
+    function generate(question) {
+    d3.tsv(question + "/results.tsv", type, function(error, data) {
+      x.domain(data.map(function(d) { return d.name; }));
+      y.domain([0, d3.max(data, function(d) { return d.value; })]);
 
-  chart.append("g")
-      .attr("class", "y axis")
-      .call(yAxis);
+      chart.append("g")
+          .attr("class", "x axis")
+          .attr("transform", "translate(0," + height + ")")
+          .call(xAxis);
 
-  chart.selectAll(".bar")
-      .data(data)
-    .enter().append("rect")
-      .attr("class", "bar")
-      .attr("x", function(d) { return x(d.name); })
-      .attr("y", function(d) { return y(d.value); })
-      .attr("height", function(d) { return height - y(d.value); })
-      .attr("width", x.rangeBand());
-});
+      chart.append("g")
+          .attr("class", "y axis")
+          .call(yAxis);
 
-function type(d) {
-  d.value = +d.value; // coerce to number
-  return d;
+      chart.selectAll(".bar")
+          .data(data)
+        .enter().append("rect")
+          .attr("class", "bar")
+          .attr("x", function(d) { return x(d.name); })
+          .attr("y", function(d) { return y(d.value); })
+          .attr("height", function(d) { return height - y(d.value); })
+          .attr("width", x.rangeBand());
+    });
+
+    
+
+    function type(d) {
+      d.value = +d.value; // coerce to number
+      return d;
+  }
 }
+   
+  generate(question)
+  index++;
+  console.log(index);
 });
+})
